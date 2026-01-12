bc20194f835162cf36b2fa35ee866ebae27e0704
abhishek-kr01 <abhishekmdp11@gmail.com>
2026-01-12 10:28:47 +0530

    Add basic Zustand store and connect it with React components

---
+++ src/components/Counter.jsx
@@ -0,0 +1,17 @@
+import {useCounterStore} from '../store/counterStore.js'
+
+function Counter() {
+
+    const {count, increase, decrease, reset} = useCounterStore();
+
+  return (
+    <div>
+        <h2>Counter: {count}</h2>
+        <button onClick={decrease}>-</button>
+        <button onClick={increase}>+</button>
+        <button onClick={reset}>Reset</button>
+    </div>
+  )
+}
+
+export default Counter
